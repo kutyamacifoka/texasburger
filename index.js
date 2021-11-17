cc807e13c51aef32d9b2bded6ebf3edb2df40877
kutyamacifoka <kutyamacifoka@gmail.com>
2021-11-17 19:23:07 +0100

    enter

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const client = contentful.createClient({
 
 // VARIABLES
 let menuGrid = document.querySelector(".menu-grid-container");
+let menuSlider = document.getElementById("menu-slider-container");
+let menuSliderTitle = document.querySelectorAll(".menu-slider-title");
 let sliderContainer = document.querySelector(".slider-container");
-
+const sliderBtns = [...document.querySelectorAll(".slider-btn")];
 let date = document.querySelector("#date");
 const homeBtn = document.querySelector(".home-btn");
 
@@ -50,30 +52,35 @@ class UI {
       // grid filter
       const gridItem = [...menuGrid.querySelectorAll(".menu-grid-img")];
 
-      gridItem.forEach((item) => {
-        item.addEventListener("mouseover", (e) => {
-          if (e.target.id === item.id) {
-            gridItem.forEach((item) => {
-              item.classList.add("grid-filter");
+      // add filter
+      function addFilter(variable) {
+        variable.forEach((item) => {
+          item.addEventListener("mouseover", (e) => {
+            if (e.target.id === item.id) {
+              variable.forEach((item) => {
+                item.classList.add("grid-filter");
+                item.classList.remove("menu-grid-clr-border");
+              });
+              item.classList.remove("grid-filter");
+              item.classList.add("menu-grid-clr-border");
+            }
+          });
+          item.addEventListener("mouseleave", () => {
+            variable.forEach((item) => {
+              item.classList.remove("grid-filter");
               item.classList.remove("menu-grid-clr-border");
             });
-            item.classList.remove("grid-filter");
-            item.classList.add("menu-grid-clr-border");
-          }
-        });
-        item.addEventListener("mouseleave", () => {
-          gridItem.forEach((item) => {
-            item.classList.remove("grid-filter");
-            item.classList.remove("menu-grid-clr-border");
           });
         });
-      });
+      }
+
+      addFilter(gridItem);
     } catch (error) {
       console.log(error);
     }
   }
 
-  // display menu slider
+  // display menu slider on load
   async displayMenuSlider() {
     try {
       let contentful = await client.getEntries({
@@ -90,13 +97,75 @@ class UI {
         })
         .map((item) => {
           return `<div class="slider">
-                    <img src="${item.image}" class="slider-img" alt="${item.title}" srcset="">
+                    <img src="${item.image}" class="slider-img" id="${item.title}" alt="${item.title}" srcset="">
                     <p class="slider-name">${item.title}</p>
                 </div>`;
         })
         .join("");
 
       sliderContainer.innerHTML = displayMenuSliderItems;
+
+      // menu slider images
+      let slider = [...document.querySelectorAll(".slider")];
+
+      // menu btn events
+      sliderBtns.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+          if (e.target.classList.contains("fa-chevron-left")) {
+            sliderContainer.insertBefore(slider[slider.length - 1], slider[0]);
+            slider = [...document.querySelectorAll(".slider")];
+          }
+          if (e.target.classList.contains("fa-chevron-right")) {
+            sliderContainer.appendChild(slider[0]);
+            slider = [...document.querySelectorAll(".slider")];
+          }
+        });
+      });
+
+      // add filter
+      function addFilter(variable) {
+        variable.forEach((item) => {
+          item.addEventListener("mouseover", () => {
+            variable.forEach((item) => {
+              item.classList.add("grid-filter");
+            });
+            item.classList.remove("grid-filter");
+          });
+          item.addEventListener("mouseleave", () => {
+            variable.forEach((item) => {
+              item.classList.remove("grid-filter");
+            });
+          });
+        });
+      }
+
+      addFilter(slider);
+
+      // display menu slider
+      function displaySlider() {
+        menuSlider.addEventListener("click", (e) => {
+          if (e.target.id == "popular") {
+            menuSliderTitle.forEach((title) => {
+              title.classList.remove("menu-active");
+              title.disabled = false;
+              e.target.classList.add("menu-active");
+              e.target.disabled = true;
+            });
+            sliderContainer.innerHTML = displayMenuSliderItems;
+          }
+          if (e.target.id == "favourite") {
+            menuSliderTitle.forEach((title) => {
+              title.classList.remove("menu-active");
+              title.disabled = false;
+              e.target.classList.add("menu-active");
+              e.target.disabled = true;
+            });
+            sliderContainer.innerHTML = "dddddddddd";
+          }
+        });
+      }
+
+      displaySlider();
     } catch (error) {
       console.log(error);
     }
