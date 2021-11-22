7aea0443cee031cffaf55cba9c9c9062e3428ac0
kutyamacifoka <kutyamacifoka@gmail.com>
2021-11-22 15:44:53 +0100

    enter

--- index.js
+++ index.js
@@ -18,27 +18,19 @@ let date = document.querySelector("#date");
 // home btn
 const homeBtn = document.querySelector(".home-btn");
 
-let favourites = [];
+let favouriteArray = [];
 
 // CLASSES
 // get products
 class Products {
-  getMenuGridItems() {}
-
-  getSliderItems() {}
-}
-
-// display products
-class UI {
-  // display menu grid items
-  async displayMenuGrid() {
+  async getMenuGridItems() {
     try {
       let contentful = await client.getEntries({
         content_type: "texasBurger",
       });
 
-      let displayMenuGridItems = await contentful.items;
-      displayMenuGridItems = displayMenuGridItems
+      let gridItems = await contentful.items;
+      gridItems = gridItems
         .map((item) => {
           const { title } = item.fields;
           const itemClass = item.fields.class;
@@ -47,178 +39,175 @@ class UI {
           return { title, itemClass, id, image };
         })
         .filter((item) => {
-          if (item.itemClass === "grid-item") {
+          if (
+            item.itemClass === "grid-item" ||
+            item.itemClass === "large-grid-item"
+          ) {
             return item;
           }
         });
-
-      let firstImgId = displayMenuGridItems[0].id;
-
-      // iterate over items
-      displayMenuGridItems = displayMenuGridItems
-        .map((item) => {
-          if (item.id == firstImgId) {
-            return `<div class="menu-grid-item lg-menu-grid-item">
-                <img src="${item.image}" class="menu-grid-img lg-grid-img" id="${item.id}" alt="${item.title}" srcset="">
-                <p class="lg-menu-grid-title" data-id="${item.title}">${item.title}</p>
-            </div>`;
-          }
-          return `<div class="menu-grid-item">
-                <img src="${item.image}" class="menu-grid-img" id="${item.id}" alt="${item.title}" srcset="">
-                <p class="menu-grid-title" data-id="${item.title}">${item.title}</p>
-            </div>`;
-        })
-        .join("");
-
-      menuGrid.innerHTML = displayMenuGridItems;
-
-      // grid filter
-      const gridItem = [...menuGrid.querySelectorAll(".menu-grid-img")];
-
-      // add filter
-      function addFilter(variable) {
-        variable.forEach((item) => {
-          item.addEventListener("mouseover", (e) => {
-            if (e.target.id === item.id) {
-              variable.forEach((item) => {
-                item.classList.add("grid-filter");
-                item.classList.remove("menu-grid-clr-border");
-              });
-              item.classList.remove("grid-filter");
-              item.classList.add("menu-grid-clr-border");
-            }
-          });
-          item.addEventListener("mouseleave", () => {
-            variable.forEach((item) => {
-              item.classList.remove("grid-filter");
-              item.classList.remove("menu-grid-clr-border");
-            });
-          });
-        });
-      }
-
-      addFilter(gridItem);
+      return gridItems;
     } catch (error) {
       console.log(error);
     }
   }
 
-  // display menu slider on load
-  async displayMenuSlider() {
+  async getSliderItems() {
     try {
       let contentful = await client.getEntries({
         content_type: "texasBurger",
       });
 
-      let displayMenuSliderItems = await contentful.items;
-      displayMenuSliderItems = displayMenuSliderItems
-        .map((item) => {
-          const { title } = item.fields;
-          const itemClass = item.fields.class;
-          const { id } = item.sys;
-          const image = item.fields.image.fields.file.url;
-          return { title, itemClass, id, image };
-        })
-        // .filter((item) => {
-        //   if (item.itemClass === "slider-item") {
-        //     return item;
-        //   }
-        // })
-        .map((item) => {
-          return `<div class="slider" id="${item.id}">
+      let sliderItems = await contentful.items;
+      sliderItems = sliderItems.map((item) => {
+        const { title } = item.fields;
+        const itemClass = item.fields.class;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, itemClass, id, image };
+      });
+      // .filter((item) => {
+      //   if (item.itemClass === "slider-item") {
+      //     return item;
+      //   }
+      // })
+      return sliderItems;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+// display products
+class UI {
+  // display grid items
+  displayMenuGridItems(gridItems) {
+    gridItems = gridItems
+      .map((item) => {
+        if (item.itemClass === "large-grid-item") {
+          return `<div class="menu-grid-item lg-menu-grid-item">
+                <img src="${item.image}" class="menu-grid-img lg-grid-img" id="${item.id}" alt="${item.title}" srcset="">
+                <p class="lg-menu-grid-title" data-id="${item.title}">${item.title}</p>
+            </div>`;
+        }
+        return `<div class="menu-grid-item">
+                <img src="${item.image}" class="menu-grid-img" id="${item.id}" alt="${item.title}" srcset="">
+                <p class="menu-grid-title" data-id="${item.title}">${item.title}</p>
+            </div>`;
+      })
+      .join("");
+
+    menuGrid.innerHTML = gridItems;
+
+    // grid filter
+    let gridImg = [...menuGrid.querySelectorAll(".menu-grid-img")];
+
+    // add filter
+    gridImg.forEach((item) => {
+      item.addEventListener("mouseover", (e) => {
+        if (e.target.id === item.id) {
+          gridImg.forEach((item) => {
+            item.classList.add("grid-filter");
+            item.classList.remove("menu-grid-clr-border");
+          });
+          item.classList.remove("grid-filter");
+          item.classList.add("menu-grid-clr-border");
+        }
+      });
+      item.addEventListener("mouseleave", () => {
+        gridImg.forEach((item) => {
+          item.classList.remove("grid-filter");
+          item.classList.remove("menu-grid-clr-border");
+        });
+      });
+    });
+  }
+
+  // display slider items
+  displayMenuSliderItems(sliderItems) {
+    sliderItems = sliderItems
+      .map((item) => {
+        return `<div class="slider" id="${item.id}">
                   <div class="star-container" id="${item.id}">
                       <i class="far fa-star favourite"></i>
                   </div>
                     <img src="${item.image}" class="slider-img" alt="${item.title}" srcset="">
                     <p class="slider-name">${item.title}</p>
                 </div>`;
-        })
-        .join("");
+      })
+      .join("");
+    sliderContainer.innerHTML = sliderItems;
 
-      sliderContainer.innerHTML = displayMenuSliderItems;
+    // menu slider images
+    let slider = [...document.querySelectorAll(".slider")];
 
-      // menu slider images
-      let slider = [...document.querySelectorAll(".slider")];
+    // menu btn events
+    sliderBtns.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        if (e.target.classList.contains("fa-chevron-left")) {
+          sliderContainer.insertBefore(slider[slider.length - 1], slider[0]);
+          slider = [...document.querySelectorAll(".slider")];
+        }
+        if (e.target.classList.contains("fa-chevron-right")) {
+          sliderContainer.appendChild(slider[0]);
+          slider = [...document.querySelectorAll(".slider")];
+        }
+      });
+    });
 
-      // menu btn events
-      sliderBtns.forEach((btn) => {
-        btn.addEventListener("click", (e) => {
-          if (e.target.classList.contains("fa-chevron-left")) {
-            sliderContainer.insertBefore(slider[slider.length - 1], slider[0]);
-            slider = [...document.querySelectorAll(".slider")];
-          }
-          if (e.target.classList.contains("fa-chevron-right")) {
-            sliderContainer.appendChild(slider[0]);
-            slider = [...document.querySelectorAll(".slider")];
-          }
+    // add filter
+    slider.forEach((item) => {
+      item.addEventListener("mouseover", () => {
+        slider.forEach((item) => {
+          item.classList.add("grid-filter");
+        });
+        item.classList.remove("grid-filter");
+      });
+      item.addEventListener("mouseleave", () => {
+        slider.forEach((item) => {
+          item.classList.remove("grid-filter");
         });
       });
+    });
 
-      // add filter
-      function addFilter(variable) {
-        variable.forEach((item) => {
-          item.addEventListener("mouseover", () => {
-            variable.forEach((item) => {
-              item.classList.add("grid-filter");
-            });
-            item.classList.remove("grid-filter");
-          });
-          item.addEventListener("mouseleave", () => {
-            variable.forEach((item) => {
-              item.classList.remove("grid-filter");
-            });
-          });
+    // choose slider container
+    menuSlider.addEventListener("click", (e) => {
+      if (e.target.id == "popular") {
+        menuSliderTitle.forEach((title) => {
+          title.classList.remove("menu-active");
+          title.disabled = false;
+          e.target.classList.add("menu-active");
+          e.target.disabled = true;
+          sliderContainer.classList.remove("hide-item");
+          popularContainer.classList.add("hide-item");
         });
       }
-
-      addFilter(slider);
-
-      // add to favourites
-      let starContainer = [...document.querySelectorAll(".star-container")];
-
-      starContainer.forEach((container) => {
-        container.addEventListener("click", (e) => {
-          let favourite = e.currentTarget.parentElement;
-          favourite.classList.toggle("favourite");
-
-          if (favourite.classList.contains("favourite")) {
-            container.innerHTML = `<i class="fas fa-star favourite"></i>`;
-          } else {
-            container.innerHTML = `<i class="far fa-star unfavourite"></i>`;
-          }
+      if (e.target.id == "favourite") {
+        menuSliderTitle.forEach((title) => {
+          title.classList.remove("menu-active");
+          title.disabled = false;
+          e.target.classList.add("menu-active");
+          e.target.disabled = true;
+          popularContainer.classList.remove("hide-item");
+          sliderContainer.classList.add("hide-item");
         });
+      }
+    });
+
+    // add to favourites
+    let starContainer = [...document.querySelectorAll(".star-container")];
+
+    starContainer.forEach((container) => {
+      container.addEventListener("click", (e) => {
+        let favourite = e.currentTarget.parentElement;
+        favourite.classList.toggle("favourite");
+        if (favourite.classList.contains("favourite")) {
+          container.innerHTML = `<i class="fas fa-star favourite"></i>`;
+        } else {
+          container.innerHTML = `<i class="far fa-star unfavourite"></i>`;
+        }
       });
-
-      // display menu slider
-      function displaySlider() {
-        menuSlider.addEventListener("click", (e) => {
-          if (e.target.id == "popular") {
-            menuSliderTitle.forEach((title) => {
-              title.classList.remove("menu-active");
-              title.disabled = false;
-              e.target.classList.add("menu-active");
-              e.target.disabled = true;
-              sliderContainer.classList.remove("hide-item");
-              popularContainer.classList.add("hide-item");
-            });
-          }
-          if (e.target.id == "favourite") {
-            menuSliderTitle.forEach((title) => {
-              title.classList.remove("menu-active");
-              title.disabled = false;
-              e.target.classList.add("menu-active");
-              e.target.disabled = true;
-              popularContainer.classList.remove("hide-item");
-              sliderContainer.classList.add("hide-item");
-            });
-          }
-        });
-      }
-
-      displaySlider();
-    } catch (error) {
-      console.log(error);
-    }
+    });
   }
 
   // show home button
@@ -244,13 +233,14 @@ class Storage {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const menuGrid = new UI();
-  const menuSlider = new UI();
-  const showDate = new UI();
-  const showHomeBtn = new UI();
+  const ui = new UI();
+  const products = new Products();
 
-  menuGrid.displayMenuGrid();
-  menuSlider.displayMenuSlider();
-  showDate.displayDate();
-  showHomeBtn.displayHomeBtn();
+  products
+    .getMenuGridItems()
+    .then((gridItems) => ui.displayMenuGridItems(gridItems))
+    .then(products.getSliderItems)
+    .then((sliderItems) => ui.displayMenuSliderItems(sliderItems))
+    .then(ui.displayHomeBtn())
+    .then(ui.displayDate());
 });
